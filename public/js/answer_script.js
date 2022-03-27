65158f30609425747023c81390a89cf2d48321ae
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-03-27 18:49:12 +0200

    Fix the issue with the JS selection

--- public/js/answer_script.js
+++ public/js/answer_script.js
@@ -4,8 +4,8 @@ Array.prototype.forEach.call(answerBtns, function (btn) {
     btn.addEventListener('click', function () {
         this.classList.toggle('bg-white');
         this.classList.toggle('selectedBtn');
-
-        this.children[0].value = true;
+        
+        this.children[0].value = this.children[0].value == "true" ? "false" : "true";
     });
 });
 
